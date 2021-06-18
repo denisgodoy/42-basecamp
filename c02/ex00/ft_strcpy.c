6fb9d0ab40e5a83ad6ad9b8637093bbdfd0f0864
denisgodoy <denisgodoy1@gmail.com>
2021-06-18 18:09:24 -0300

    c02 evaluation 55/100

---
+++ c02/ex00/ft_strcpy.c
@@ -0,0 +1,27 @@
+int	ft_strlen(char	*str);
+
+char	*ft_strcpy(char	*dest, char	*src)
+{
+	int	i;
+
+	i = 0;
+	while (i < ft_strlen(src))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int	ft_strlen(char	*str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
