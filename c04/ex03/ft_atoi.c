4ef93d7bc7e036825882096ce08f15bc45be47bc
denisgodoy <denisgodoy1@gmail.com>
2021-07-01 11:00:41 -0300

    atoi corrected to return int if next char is not valid

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -9,17 +9,17 @@ int	ft_atoi(char	*str)
 	i = 0;
 	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	while (str[i] == '+' || str[i] == '-' || (str[i] >= '0' && str[i] <= '9'))
+	while (str[i] == '+' || str[i] == '-')
 	{
-		if (str[i] == '+' || str[i] == '-')
-		{
-			if (str[i] == '-')
-				count_minus += 1;
-		}
-		else
-			dest = dest * 10 + (str[i] - 48);
-		i++;
+        if (str[i] == '-')
+            count_minus += 1;
+        i++;
 	}
+	while (str[i] >= '0' && str[i] <= '9')
+    {
+        dest = dest * 10 + (str[i] - 48);
+        i++;
+    }
 	if (count_minus % 2 != 0)
 		return (dest *= -1);
 	return (dest);
